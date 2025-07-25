66f9ff15caccb5c01a4c3bec6f3a656dc9a11bec
bagdenius <bagdensparrow@gmail.com>
2025-07-25 17:55:48 +0300

    feat: add order loading functionality and search feature; update routing to include loaders

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const SearchOrder = () => {
+  const [query, setQuery] = useState(``);
+  const navigate = useNavigate();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (!query) return;
+    navigate(`/order/${query}`);
+    setQuery(``);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Search order number"
+        value={query}
+        onChange={(event) => setQuery(event.target.value)}
+      />
+    </form>
+  );
+};
+
+export default SearchOrder;
