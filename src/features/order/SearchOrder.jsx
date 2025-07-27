53b37677ad6ca52bef5428095a3c16c9b9e60f41
bagdenius <bagdensparrow@gmail.com>
2025-07-27 22:20:38 +0300

    feat: enhance styling in CreateOrder and SearchOrder components; improve input styles and add classes for better UI

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const SearchOrder = () => {
@@ -18,7 +18,7 @@ const SearchOrder = () => {
         placeholder="Search order number"
         value={query}
         onChange={(event) => setQuery(event.target.value)}
-        className="bg-white"
+        className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:ring focus:ring-yellow-500/50 focus:outline-none sm:w-64 sm:focus:w-72"
       />
     </form>
   );
