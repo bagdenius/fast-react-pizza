5c3e733274c4271a7848a0644b3af8376e89cc13
bagdenius <bagdensparrow@gmail.com>
2025-07-27 18:59:58 +0300

    feat: integrate Tailwind CSS and update styles across components
    - Added Tailwind CSS and its Vite plugin to the project.
    - Updated package.json to include Tailwind CSS and Prettier plugins.
    - Refactored CartOverview, OrderItem, Header, Home, and added Username component with Tailwind CSS classes for improved styling.
    - Modified index.css to import Tailwind CSS and commented out previous styles.
    - Created a new Username component to display the user's name.
    - Configured Prettier with Tailwind CSS plugin for consistent code formatting.

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Username = () => {
+  return <div className="text-sm font-semibold">Bohdan</div>;
+};
+
+export default Username;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,13 +1,15 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
+import Username from '../features/user/Username';
 
 const Header = () => {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
-      <p>Bohdan</p>
+      <Username />
     </header>
   );
 };
