8bc50625861107fbcf2486b8edbbfc5cbd3d92d3
bagdenius <bagdensparrow@gmail.com>
2025-07-27 19:45:55 +0300

    feat: enhance layout and styling; add flexbox to CartOverview, Header, and update spacing in Home component

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const SearchOrder = () => {
   const [query, setQuery] = useState(``);
@@ -18,6 +18,7 @@ const SearchOrder = () => {
         placeholder="Search order number"
         value={query}
         onChange={(event) => setQuery(event.target.value)}
+        className="bg-white"
       />
     </form>
   );
